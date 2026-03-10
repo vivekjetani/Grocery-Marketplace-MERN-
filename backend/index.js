9c2a48f1d0b4395743a6de7c09ba2039e2c17402
Vivek jetani <jetanivivek321@gmail.com>
2026-03-11 03:31:48 +0530

    feat: Initialize the backend server with Express, configure middleware, define API routes, connect to services, and schedule cron jobs.

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,7 @@ const app = express();
 await connectCloudinary();
 // allow multiple origins
 const allowedOrigins = [
+  "https://gramodaya.vercel.app",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:5174",
